23659448abf2f7c4c16831577b840b4c2ba78ff8
domjanb <domjan.balazs@outlook.com>
2018-10-02 15:47:54 +0200

    Add project files.

---
+++ Bevezetes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bevezetes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
