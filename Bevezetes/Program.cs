bf8d5590e906d3f503d03f9adc3d0447f5312742
domjanb <domjan.balazs@outlook.com>
2018-10-03 12:25:33 +0200

    proba2

--- Bevezetes/Program.cs
+++ Bevezetes/Program.cs
@@ -8,6 +8,7 @@ namespace Bevezetes
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!2");
+            Console.WriteLine("Hello World!3");
             Console.ReadLine();
         }
     }
