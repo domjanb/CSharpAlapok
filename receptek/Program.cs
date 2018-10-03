08f67c25e8e822a12f0411e2ff5d9cc1cbf4d1ef
domjanb <domjan.balazs@outlook.com>
2018-10-03 12:43:18 +0200

    #2 receptek is

---
+++ receptek/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace receptek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hus");
+            Console.WriteLine("hagyma");
+            Console.WriteLine("paprika");
+        }
+    }
+}
