dde6df2acae7240685c05aeb4406c282cde14e7f
domjanb <domjan.balazs@outlook.com>
2018-10-03 13:22:05 +0200

    cserebere

--- receptek/Program.cs
+++ receptek/Program.cs
@@ -6,9 +6,22 @@ namespace receptek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hus");
-            Console.WriteLine("hagyma");
-            Console.WriteLine("paprika");
+            var ma = DateTime.Today;
+            var naneve = ma.DayOfWeek;
+            if (naneve == DayOfWeek.Friday)
+            {
+                Console.WriteLine("szerva van");
+            }
+            else
+            {
+                Console.WriteLine(ma);
+                Console.WriteLine("hus");
+                Console.WriteLine("hagyma");
+                Console.WriteLine("paprika");
+            }
+            
+            Console.ReadLine();
         }
+        
     }
 }
