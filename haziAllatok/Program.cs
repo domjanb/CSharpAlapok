cfc83de08b5faa9836da3cb374c60dbcbe2cff32
domjanb <domjan.balazs@outlook.com>
2018-10-05 12:51:06 +0200

    kutya macsak javitgat

--- haziAllatok/Program.cs
+++ haziAllatok/Program.cs
@@ -7,12 +7,14 @@ namespace haziAllatok
         static void Main(string[] args)
         {
             var kutya = new Kutya();
+            Console.WriteLine("kutya-kutya");
             kutya.Koszon();
             kutya.Enekel();
             kutya.Beszel();
             Console.WriteLine("\n");
 
             Kutya kutya2 = new Kutya();
+            Console.WriteLine("kutya-kutya");
             kutya2.Koszon();
             kutya2.Enekel();
             kutya2.Beszel();
@@ -21,16 +23,42 @@ namespace haziAllatok
 
 
             var haziAllat = new HaziAllat();
+            Console.WriteLine("haziallat-haziallat");
             haziAllat.Koszon();
             haziAllat.Enekel();
             haziAllat.Beszel();
             Console.WriteLine("\n");
 
-            var haziAllat2 = new Kutya();
-            haziAllat2.Koszon();
-            haziAllat2.Enekel();
-            haziAllat2.Beszel();
+            HaziAllat haziAllat1 = new Kutya();
+            Console.WriteLine("haziallat-kuzya");
+            haziAllat1.Koszon();
+            haziAllat1.Enekel();
+            haziAllat1.Beszel();
             Console.WriteLine("\n");
+
+            Console.WriteLine("haziallat(kutya) cast kuzya");
+            ((Kutya)haziAllat1).Koszon();
+            ((Kutya)haziAllat1).Enekel();
+            ((Kutya)haziAllat1).Beszel();
+            Console.WriteLine("\n");
+
+
+
+            var haziAllat2 = new Kutya();
+            //kulya tipusnakveszem a háziállat felüleltét
+            Console.WriteLine("kutya cast haziallat");
+            ((HaziAllat)haziAllat2).Koszon();
+            ((HaziAllat)haziAllat2).Enekel();
+            ((HaziAllat)haziAllat2).Beszel();
+            Console.WriteLine("\n");
+
+            //nem lehet
+            /*var haziAllat3 = new HaziAllat();
+            Console.WriteLine("haziallat cast kutya");
+            ((Kutya)haziAllat3).Koszon();
+            ((Kutya)haziAllat3).Enekel();
+            ((Kutya)haziAllat3).Beszel();
+            Console.WriteLine("\n");*/
 
 
             Console.ReadLine();
@@ -61,6 +89,7 @@ namespace haziAllatok
         public override void Beszel()
         {
             Console.WriteLine("kuzya beszel ");
+            base.Beszel();
         }
 
     }
