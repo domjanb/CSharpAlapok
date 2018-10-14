b26c805bf15360be45fa63c21dc71dadcdd73129
domjan.balazs <domjan.balazs@outlook.com>
2018-10-14 20:45:59 +0200

    serializalas is

---
+++ Serializalas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializalas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adat = new AdatOsztaly() {
+                Egesz = int.MaxValue,
+                TizedesTort = 12.728m,
+                DatumIdo = DateTime.MaxValue,
+                Datummin = DateTime.MinValue,
+                Szoveg = "ÁrvízTüro"
+
+            };
+            var fileNeve = "tesztfile.txt";
+            var serializer = new XmlSerializer(typeof(AdatOsztaly));
+            using (var fs = new FileStream(fileNeve, FileMode.Create))
+            {
+                
+                serializer.Serialize(fs, adat);
+            }
+            using (var fs = new FileStream(fileNeve, FileMode.Open))
+            {
+
+                var beolvasott= serializer.Deserialize(fs);
+                Console.WriteLine(beolvasott);
+                Console.ReadLine();
+            }
+        }
+
+    }
+    public class AdatOsztaly
+    {
+        public int Egesz { get; set; }
+        public decimal TizedesTort { get; set; }
+        public DateTime DatumIdo { get; set; }
+        public DateTime Datummin { get; set; }
+        public string Szoveg { get; set; }
+
+
+    }
+}
---
+++ StreamString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var fileName = "teszt.txt";
+            File.WriteAllText(fileName,
+                string.Format("file tartalom. \n vagy paaméterek {0} is irhatóak, {0} további pec karakterek {1}, {2}, {3}, {4}",
+                Environment.NewLine,
+                (char)113, //ASCII karakter
+                Convert.ToChar(113), //ASCII karakter
+                '\u0027',
+                //byte tombot karakterkodokkal stringgé konvertálva
+                new string(Encoding.ASCII.GetChars(new byte[] {35,36 }))
+                ),
+                Encoding.UTF8
+                );
+            using (var fs = new FileStream(fileName, FileMode.Open))
+          
+ 
+ {
+                using (var sr = new StreamReader(fs, Encoding.UTF8)) 
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var text = sr.ReadLine();
+                        Console.WriteLine(text);
+                    }
+                    
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
